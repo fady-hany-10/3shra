7197adfac9c0021dc523d1303ce925bfa489fbb4
fady hany <fadyhanyyy7@gmail.com>
2026-09-11 07:28:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,31 @@
 const config = {
-    title: "White for woman",
-    subtitle: " Welcome to my page",
-    logo: "logo.png",
+    title: "عِشره",
+    subtitle: "Art Workshops & Creative Moments 🖌️",
+    logo: "logo (6).png",
     links: [
         {
-            name: "Instagram",
-            url: "https://www.instagram.com/white.for.women?stkn=MTBhenAwZmtvOTR5dg==",
-            icon: "fa-brands fa-instagram",
-            class: "btn-instagram"
-        },
-        {
             name: "Facebook",
-            url: "https://www.facebook.com/share/1CSEAmQhSR/?mibextid=wwXIfr",
+            url: "https://www.facebook.com/share/19NNyaVXsw/?mibextid=wwXIfr",
             icon: "fa-brands fa-facebook-f",
             class: "btn-facebook"
         },
         {
+            name: "Instagram",
+            url: "https://www.instagram.com/eshra.events?stkn=YWV0N2kzNDMwbGRr",
+            icon: "fa-brands fa-instagram",
+            class: "btn-instagram"
+        },
+        {
             name: "TikTok",
-            url: "https://www.tiktok.com/@white.for.women?_r=1&_t=ZS-99bIeLph8FN",
+            url: "https://www.tiktok.com/@3eshra_?_r=1&_t=ZS-99dNuI45xeD",
             icon: "fa-brands fa-tiktok",
             class: "btn-tiktok"
         },
         {
-            name: "InstaPay",
-            url: "https://ipn.eg/S/moazkishk/instapay/3NfHks",
-            icon: "fa-solid fa-wallet",
-            class: "btn-instapay"
+            name: "Orange Cash",
+            url: "tel:01289540795",
+            icon: "fa-solid fa-mobile-screen-button",
+            class: "btn-orange"
         }
     ]
 };
